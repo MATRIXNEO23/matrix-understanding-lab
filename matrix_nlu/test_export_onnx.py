202b4ff0b431a91ca5da0aac2380a46b7580d827
MATRIXNEO23 <101059510+MATRIXNEO23@users.noreply.github.com>
2026-09-04 16:09:45 +0200

    Add tests for mixed head-protected quantization policy

--- matrix_nlu/test_export_onnx.py
+++ matrix_nlu/test_export_onnx.py
@@ -1,6 +1,7 @@
 import unittest
 
 import export_onnx
+import export_mixed_head_protected_onnx as mixed_export
 
 
 class OnnxExportContractTest(unittest.TestCase):
@@ -15,6 +16,38 @@ class OnnxExportContractTest(unittest.TestCase):
     def test_percentile_is_deterministic(self):
         self.assertEqual(5, export_onnx.percentile([5, 1, 4, 2, 3], .95))
         self.assertEqual(1, export_onnx.percentile([5, 1, 4, 2, 3], 0))
+
+    def test_mixed_head_protected_policy_covers_matrix_heads(self):
+        for output in (
+            "token.negation",
+            "token.object",
+            "token.subject",
+            "token.temporal",
+            "sequence.dialogueAct",
+            "sequence.predicate",
+            "sequence.subjectReferent",
+            "sequence.targetReferent",
+            "sequence.ownerReferent",
+            "sequence.perspectiveReferent",
+            "sequence.polarity",
+            "sequence.temporalRelation",
+        ):
+            self.assertIn(output, mixed_export.PROTECTED_OUTPUTS)
+        self.assertNotIn("massive.intent", mixed_export.PROTECTED_OUTPUTS)
+        self.assertNotIn("massive.slot", mixed_export.PROTECTED_OUTPUTS)
+
+    def test_mixed_head_protected_status_is_experimental(self):
+        self.assertEqual(
+            "EXPERIMENTAL_TEST_CANDIDATE_NOT_PRODUCTION_APPROVED",
+            mixed_export.EXPERIMENTAL_STATUS,
+        )
+
+    def test_flatten_grouped_nodes_is_stable_and_unique(self):
+        grouped = {
+            "sequence_heads.predicate": ["nodeA", "nodeB"],
+            "sequence_heads.polarity": ["nodeB", "nodeC"],
+        }
+        self.assertEqual(["nodeA", "nodeB", "nodeC"], mixed_export.flatten_grouped_nodes(grouped))
 
 
 if __name__ == "__main__":
